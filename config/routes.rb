42ce88abd2fe72b5fb6abef8eb0c7e182c438ff6
Marti D <me@martidolce.com>
2021-01-29 16:37:11 -0500

    Fixed routes.
    index, controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :coupons
+  resources :coupons, only: [:index, :show, :create, :new]
+
 
 end
