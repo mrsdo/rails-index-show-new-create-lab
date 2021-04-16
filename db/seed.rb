25529388e36c208fd44619d56997d4491d08b028
Marti D <me@martidolce.com>
2021-04-16 12:51:24 -0400

    Tests passing

--- db/seed.rb
+++ db/seed.rb
@@ -1,4 +1,24 @@
-Dir[Rails.root.join('db/seeds/*.rb')].sort.each do |file|
-  puts "Processing #{file.split('/').last}"
-  require file
-end
+
+# Dir[Rails.root.join('db/seeds/*.rb')].sort.each do |file|
+#   puts "Processing #{file.split('/').last}"
+#   require file
+# end
+#
+
+# Coupon.destroy_all
+
+Coupon.create!([{
+                  coupon_code: "JBS123",
+                  store: "Jet Brains"
+                },
+                {
+                  coupon_code: "WDS123",
+                  store: "WordPress"
+                },
+                {
+                  coupon_code: "ENV123",
+                  store: "Envato"
+                }])
+
+p "Created #{Coupon.count} coupons"
+
